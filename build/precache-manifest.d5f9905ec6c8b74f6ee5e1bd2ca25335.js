527ef0b43af4234fc3d22095412e7758f64a6e55
VillePajala <valoraami@gmail.com>
2019-06-10 13:04:23 +0300

    added UI

---
+++ build/precache-manifest.d5f9905ec6c8b74f6ee5e1bd2ca25335.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552b152143cea3805cad64967836420c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9496d13fd431211f0423",
+    "url": "/static/css/main.c63029ef.chunk.css"
+  },
+  {
+    "revision": "23e85acd532ab5c8eb07",
+    "url": "/static/js/2.e4ca2d3b.chunk.js"
+  },
+  {
+    "revision": "9496d13fd431211f0423",
+    "url": "/static/js/main.af229d18.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
