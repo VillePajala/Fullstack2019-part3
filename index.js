d6cc38294e98ed7bd9eb9e47a01a7c09d6cfd055
VillePajala <valoraami@gmail.com>
2019-06-12 21:22:21 +0300

    uibuild

--- index.js
+++ index.js
@@ -17,22 +17,48 @@ morgan.token('data', (req) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 
-app.get('/api/persons', (req, res) => {
-  Person.find({}).then(persons => {
-    res.json(persons.map(person => person.toJSON()))
+app.get('/api/persons', (req, res, next) => {
+  Person.find({})
+    .then(persons => {
+      res.json(persons.map(person => person.toJSON()))
   })
+  .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  Person.findById(req.params.id).then(person => {
-    res.json(person.toJSON())
-  })
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person.toJSON())
+      } else {
+        res.status(404).end()
+      }
+      
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body
+  
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  console.log(person)
+  Person.findByIdAndUpdate(req.params.id, person, { new: true})
+    .then(updatedPerson => {
+      res.json(updatedPerson.toJSON())
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
@@ -44,23 +70,25 @@ app.post('/api/persons', (req, res) => {
     })
   } 
 
-  Person.find({}).then(persons => {
-    const allNames = persons.map(person => person.name)
-    if (allNames.includes(body.name)) {
-      return res.status(400).json({
-        error: 'name must be unique'
-      })
-    } 
+  Person.find({})
+    .then(persons => {
+      const allNames = persons.map(person => person.name)
+      if (allNames.includes(body.name)) {
+        return res.status(400).json({
+          error: 'name must be unique'
+        })
+      } 
 
-    const person = new Person( {
-      name: body.name,
-      number: body.number,
-    })
+      const person = new Person( {
+        name: body.name,
+        number: body.number,
+      })
   
-    person.save().then(savedPerson => {
-      res.json(savedPerson.toJSON())
+      person.save().then(savedPerson => {
+        res.json(savedPerson.toJSON())
+      })
     })
-  })
+    .catch(error => next(error))
 })
 
 app.get('/info', (req, res) => {
@@ -69,6 +97,24 @@ app.get('/info', (req, res) => {
               ${new Date()}
             </div>`)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
